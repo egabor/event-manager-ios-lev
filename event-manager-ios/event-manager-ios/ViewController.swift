e3772687e31512858e3df6030cc3353f5eb3fb5a
Eszenyi Gabor <eszenyi.gabor@icloud.com>
2017-10-01 20:42:58 +0200

    🚧 Empty project created

---
+++ event-manager-ios/event-manager-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  event-manager-ios
+//
+//  Created by Career Mode on 2017. 10. 01..
+//  Copyright © 2017. Gabor Eszenyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
