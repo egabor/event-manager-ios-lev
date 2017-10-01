76c8887ec9d875a111115df421f5701307439abf
Eszenyi Gabor <eszenyi.gabor@icloud.com>
2017-10-01 21:04:36 +0200

    ✨ LogIn files added.

---
+++ event-manager-ios/event-manager-ios/Modules/LogIn/LogInViewController.swift
@@ -0,0 +1,68 @@
+//
+//  LogInViewController.swift
+//  event-manager-ios
+//
+//  Created by Career Mode on 2017. 10. 01..
+//  Copyright © 2017. Gabor Eszenyi. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class LogInViewController: UIViewController {
+
+    // MARK: - let constants
+
+    let disposeBag = DisposeBag()
+
+    // The viewmodel must be let!
+    // To prevent memory leaks change the model inside the viewmodel instead of changing the viewmodel object.
+    let viewModel = LogInViewModel()
+
+    // MARK: - var variables
+
+    // MARK: - Interface Builder Outlets
+
+    // MARK: - UIViewController Lifecycle Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO: Do the viewmodel binding here
+    }
+
+    deinit {
+        // Don't forget to remove the observers here
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: - Helper Methods
+
+}
+
+// MARK: - Interface Builder Actions
+
+extension LogInViewController {
+
+}
+
+// MARK: - Notification handlers can be placed here
+
+extension LogInViewController {
+
+}
---
+++ event-manager-ios/event-manager-ios/Modules/LogIn/LogInViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  LogInViewModel.swift
+//  event-manager-ios
+//
+//  Created by Career Mode on 2017. 10. 01..
+//  Copyright © 2017. Gabor Eszenyi. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+class LogInViewModel {
+
+    // MARK: - let constants
+
+    // MARK: - var variables
+
+    // MARK: - Lifecycle Methods
+
+    init () {
+
+    }
+
+    deinit {
+        // Don't forget to remove the observers here
+    }
+
+    // MARK: - Busniness Logic
+
+    // MARK: - Helper Methods
+
+}
+
+// MARK: - Notification handlers can be placed here
+
+extension LogInViewModel {
+
+}
