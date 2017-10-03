242f8e33b40b42b869973566fb108ae3620e07f4
Eszenyi Gabor <eszenyi.gabor@icloud.com>
2017-10-03 22:10:25 +0200

    🚧 Constants added.

---
+++ event-manager-ios/Utilities/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  event-manager-ios
+//
+//  Created by Eszenyi Gábor on 2017. 10. 03..
+//  Copyright © 2017. Gabor Eszenyi. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+	struct Notifications {
+		static let StartLoadingIndicatorAnimation = NSNotification.Name(rawValue: "StartLoadingIndicatorAnimation")
+		static let StopLoadingIndicatorAnimation = NSNotification.Name(rawValue: "StopLoadingIndicatorAnimation")
+	}
+}
