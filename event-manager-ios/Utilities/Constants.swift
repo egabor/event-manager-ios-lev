91847a1989108b2662539de30669bbde27b301a5
Eszenyi Gabor <eszenyi.gabor@icloud.com>
2017-10-03 23:38:41 +0200

    🚧 Notification constants added.

--- event-manager-ios/Utilities/Constants.swift
+++ event-manager-ios/Utilities/Constants.swift
@@ -13,5 +13,7 @@ struct Constants {
 	struct Notifications {
 		static let StartLoadingIndicatorAnimation = NSNotification.Name(rawValue: "StartLoadingIndicatorAnimation")
 		static let StopLoadingIndicatorAnimation = NSNotification.Name(rawValue: "StopLoadingIndicatorAnimation")
+		static let ShowLoadingAnimationView = NSNotification.Name(rawValue: "ShowLoadingAnimationView")
+		static let HideLoadingAnimationView = NSNotification.Name(rawValue: "HideLoadingAnimationView")
 	}
 }
