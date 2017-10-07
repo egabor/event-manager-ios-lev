4c56c313728d00d9f422ae3df96f05c0022f4994
egabor <eszenyi_gabor@yahoo.com>
2017-10-07 12:22:01 +0200

    🚧 Viewcontroller and storyboard cahnges.

---
+++ event-manager-ios/Shared/Models/Event.swift
@@ -0,0 +1,19 @@
+//
+//  Event.swift
+//  event-manager-ios
+//
+//  Created by Eszenyi Gábor on 2017. 10. 07..
+//  Copyright © 2017. Gabor Eszenyi. All rights reserved.
+//
+
+import Foundation
+
+class Event: Bindable {
+
+    // MARK: - Model Properties
+    var eventId: String!
+
+    init () {
+
+    }
+}
