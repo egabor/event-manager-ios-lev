48f4d358192c3eaebdbb324bc7926734fdd607dd
egabor <eszenyi_gabor@yahoo.com>
2017-11-11 15:30:00 +0100

    🚧 Controller added to MapOptions.

---
+++ event-manager-ios/Modules/MapOptions/MapOptionsViewModel.swift
@@ -0,0 +1,44 @@
+//
+//  MapOptionsViewModel.swift
+//  event-manager-ios
+//
+//  Created by Eszenyi Gábor on 2017. 11. 11..
+//  Copyright © 2017. Gabor Eszenyi. All rights reserved.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+import RxDataSources
+
+class MapOptionsViewModel {
+
+    // MARK: - let constants
+
+    // MARK: - var variables
+
+    // Change the sections variable to update the TableView
+    var sections = Variable([TableViewSection]())
+    var dataSource = RxTableViewSectionedReloadDataSource<TableViewSection>()
+
+    // MARK: - Lifecycle Methods
+
+    init () {
+
+    }
+
+    deinit {
+        // Don't forget to remove the observers here
+    }
+
+    // MARK: - Business Logic
+
+    // MARK: - Helper Methods
+
+}
+
+// MARK: - Notification handlers can be placed here
+
+extension MapOptionsViewModel {
+
+}
