5fd40354999e297807cb52008fc328768abbc0b4
egabor <eszenyi_gabor@yahoo.com>
2017-12-14 22:55:36 +0100

    🚧 Test function.

--- api/functions/index.js
+++ api/functions/index.js
@@ -1,8 +1,9 @@
-const functions = require('firebase-functions');
+var functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+
+exports.editProfile = functions.https.onRequest((req, res) => {
+    response.send("Test");
+});
