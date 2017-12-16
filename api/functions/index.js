00144c6cd76f4f75093fcec494cdd4b0033da37e
egabor <eszenyi_gabor@yahoo.com>
2017-12-16 14:56:32 +0100

    🚧 Get profile.

--- api/functions/index.js
+++ api/functions/index.js
@@ -5,7 +5,8 @@ admin.initializeApp(functions.config().firebase);
 
 
 exports.editProfile = functions.https.onRequest((req, res) => {
-    var userId = req.get('Auth-Token')
+    var userId = req.get('UserId')
+    var authToken = req.get('Auth-Token');
     let billingName;
     let billingZipCode;
     let billingCity;
@@ -33,15 +34,14 @@ exports.editProfile = functions.https.onRequest((req, res) => {
         break;
     }
     return admin.database().ref('users/' + userId + '/userData').once('value', (snapshot) => {
-        if (snapshot.val()) {
+        if (snapshot.val() && snapshot.val().authToken == authToken) {
             admin.database().ref('users/' + userId + '/userData').update(dict);
 
-        admin.database().ref('users/' + userId + '/userData').once('value', (snapshot) => {
-            var userData = snapshot.val();
-            userData.authToken = snapshot.key;
-            res.send(userData);
-            
-         });
+            admin.database().ref('users/' + userId + '/userData').once('value', (snapshot) => {
+                var userData = snapshot.val();
+                userData.userId = userId;
+                res.send(userData);
+            });
         } else {
             let errors = {}
             errors.message = "Forbidden access."
@@ -52,13 +52,27 @@ exports.editProfile = functions.https.onRequest((req, res) => {
   });
 
 
-  exports.getProfile = functions.https.onRequest((req, res) => {
-    var userId = req.get('Auth-Token')
+  exports.users = functions.https.onRequest((req, res) => {
+      console.log(req.url.split('/')[1]);
+
+    var urlUserId = req.url.split('/')[1]
+    var userId = ''
+    var authToken = req.get('Auth-Token');
+
+    if (urlUserId == 'me') {
+        userId = req.get('UserId');
+    } else if (urlUserId) {
+        userId = urlUserId;
+    }
+
     
     return admin.database().ref('users/' + userId + '/userData').once('value', (snapshot) => {
-        if (snapshot.val()) {
-            var userData = snapshot.val();
-            userData.userId = snapshot.key;
+        var userData = snapshot.val();
+        if (snapshot.val() && snapshot.val().authToken == authToken) {
+            userData.userId = userId;
+            if (urlUserId != 'me') {
+                userData.authToken = null;
+            }
             res.send(userData);
         } else {
             let errors = {}
